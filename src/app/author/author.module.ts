b9d6c9f978e2ff7c8a730c0114b13725ccfda0dc
Erick Bogarin <erick.bogarin@indt.org.br>
2017-07-15 15:26:57 -0400

    Add author create/update routes
    Signed-off-by: Erick Bogarin <erick.bogarin@indt.org.br>

--- src/app/author/author.module.ts
+++ src/app/author/author.module.ts
@@ -1,15 +1,19 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { SharedModule } from '../shared';
+import { AuthorResolver } from './author-resolver.service';
 import { AuthorListComponent } from './author-list/author-list.component';
 import { AuthorFormComponent } from './author-form/author-form.component';
-import { SharedModule } from '../shared';
+import { AuthorCreateComponent } from './author-form/author-create/author-create.component';
+import { AuthorUpdateComponent } from './author-form/author-update/author-update.component';
 
 const authorRouting: ModuleWithProviders = RouterModule.forChild([
   {
     path: 'authors',
     children: [
-      { path: 'create', component: AuthorFormComponent }
+      { path: 'create', component: AuthorCreateComponent },
+      { path: ':id/update', component: AuthorUpdateComponent, resolve: { author: AuthorResolver } }
     ]
   }
 ]);
@@ -21,10 +25,15 @@ const authorRouting: ModuleWithProviders = RouterModule.forChild([
   ],
   declarations: [
     AuthorListComponent,
-    AuthorFormComponent
+    AuthorFormComponent,
+    AuthorCreateComponent,
+    AuthorUpdateComponent
   ],
   exports: [
     AuthorListComponent
+  ],
+  providers: [
+    AuthorResolver
   ]
 })
 export class AuthorModule {}
