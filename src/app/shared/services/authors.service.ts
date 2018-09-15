d64fecbec11c9845e39d79b42bc3d0a0c25afd22
Erick Bogarin <erick.bogarin@gmail.com>
2018-09-15 11:22:13 -0400

    Refactor rxjs operators in a single file
    Signed-off-by: Erick Bogarin <erick.bogarin@gmail.com>

--- src/app/shared/services/authors.service.ts
+++ src/app/shared/services/authors.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { URLSearchParams } from '@angular/http';
 
 import { Observable } from 'rxjs/Rx';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
 
 import { ApiService } from './api.service';
 import { Author, AppQueryParams, APIResponse } from '../../shared';
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,27 +4,17 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
-import { DropdownDirective } from './directives/dropdown.directive';
-import { PaginationComponent } from './components/pagination/pagination.component';
 import { AlertComponent } from './components/alert/alert.component';
 import { ModalComponent } from './components/modal/modal.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { DropdownDirective } from './directives/dropdown.directive';
 import { SortByDirective } from './directives/sort-by.directive';
 
+import './rxjs-operators';
+
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpModule,
-    RouterModule
-  ],
-  declarations: [
-    DropdownDirective,
-    PaginationComponent,
-    AlertComponent,
-    SortByDirective,
-    ModalComponent
-  ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, HttpModule, RouterModule],
+  declarations: [DropdownDirective, PaginationComponent, AlertComponent, SortByDirective, ModalComponent],
   exports: [
     CommonModule,
     FormsModule,
