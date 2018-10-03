eb0268a67d28c6adf4c0564a91cf001ef1841188
Erick Bogarin <erick.bogarin@gmail.com>
2018-10-02 21:47:49 -0400

    Add ng-boostrap depedency
    Signed-off-by: Erick Bogarin <erick.bogarin@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { ModalModule } from 'ngx-bootstrap/modal';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
@@ -20,7 +18,7 @@ import {
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, SharedModule, ModalModule.forRoot(), AppRoutingModule, HomeModule],
+  imports: [BrowserModule, SharedModule, AppRoutingModule, HomeModule],
   providers: [ApiService, AuthorsService, PaginationService, AlertService, SortByService, ModalService],
   bootstrap: [AppComponent]
 })
